988a39baf2df2a33b5880ee145a97bc20939d432
kamomathiane <kamomathiane99@gmail.com>
2025-05-14 12:15:09 +0200

    module and student

---
+++ Missing_Middle_Student.Model/Models/StudentModule.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Reflection;
+
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Missing_Middle_Student.Model
+{
+    public class StudentModule
+    {
+        public int StudentNum { get; set; }
+        [ForeignKey("StudentNum")]
+        public Student Student { get; set; }
+
+        public string ModuleCode { get; set; }
+        [ForeignKey("ModuleCode")]
+        public Module Module { get; set; }
+
+        public int Year { get; set; } // Optional: for tracking year enrolled
+        public string Semester { get; set; } // Optional: "First", "Second"
+
+        // ✅ New field for the grade/mark the student got
+        public double Mark { get; set; }
+    }
+}
+
--- Missing_Middle_Student.Services/obj/Debug/net8.0/Missing_Middle_Student.Services.AssemblyInfo.cs
+++ Missing_Middle_Student.Services/obj/Debug/net8.0/Missing_Middle_Student.Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Missing_Middle_Student.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7df0e2da15886f680be8636b3e41d39061b02239")]
 [assembly: System.Reflection.AssemblyProductAttribute("Missing_Middle_Student.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Missing_Middle_Student.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
